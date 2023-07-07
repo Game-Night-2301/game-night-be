f8c1f96897f6bcadd80d04494b273037610e21f9
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-07-07 16:17:07 -0600

    initialize app and add graphql

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
