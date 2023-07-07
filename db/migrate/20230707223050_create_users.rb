adfe53f71735539f3bd185d1977b5eb136e54fd4
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-07-07 16:40:13 -0600

    migrations

---
+++ db/migrate/20230707223050_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :city
+      t.float :lat
+      t.float :lon
+
+      t.timestamps
+    end
+  end
+end
