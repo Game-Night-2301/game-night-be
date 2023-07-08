d7324908dca26e3929d79c49ad9100aaabbefe8a
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-07-08 14:57:57 -0600

    feat: Add attending events and hosted events to user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -9,8 +9,17 @@ module Types
     field :state, String
     field :lat, Float
     field :lon, Float
-    field :events, [Types::EventType]
+    field :attending_events, [Types::EventType]
+    field :hosted_events, [Types::EventType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def attending_events
+      object.events
+    end
+    
+    def hosted_events
+      object.events.where(host_id: object.id)
+    end
   end
 end
