640c3a970bd1984d989cb86b44df95d4810060e7
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-07-09 12:04:48 -0600

    test: Sad path for creating an event

--- spec/graphql/mutations/events/create_an_event_spec.rb
+++ spec/graphql/mutations/events/create_an_event_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 module Mutations
   module Events
-
     RSpec.describe CreateEvent, type: :request do
       describe 'resolve' do
         it 'can create an event' do
-          post '/graphql', params: { query: query }
-      
+          post '/graphql', params: { query: }
+
           result = JSON.parse(response.body)
           event = Event.last
 
@@ -22,38 +23,78 @@ module Mutations
           expect(result['data']['createEvent']['event']['game']).to eq(event.game)
           expect(result['data']['createEvent']['event']['gameType']).to eq(event.game_type)
         end
+
+        it 'returns an error if missing a required field' do
+          post '/graphql', params: { query: query_missing_required_field }
+
+          result = JSON.parse(response.body)
+
+          expect(result['errors'][0]['message']).to eq("Argument 'city' on InputObject 'CreateEventInput' is required. Expected type String!")
+          expect(result['errors'][1]['message']).to eq("Argument 'zip' on InputObject 'CreateEventInput' is required. Expected type Int!")
+        end
       end
-    
+
       def query
         <<~GQL
-        mutation {
-          createEvent(input: {
-            date: "2023/11/7",
-            address: "123 This street",
-            state: "Colorado",
-            city: "Denver",
-            zip: 15555,
-            title: "Grandma's basement",
-            description: "We'll be playing caracasonne for 9 hours",
-            host: 4744564,
-            game: 97833646,
-            gameType: "board game"
-          }) {
-            event {
-              id
-              date
-              address
-              state
-              city
-              zip
-              title
-              description
-              hostId
-              game
-              gameType
+          mutation {
+            createEvent(input: {
+              date: "2023/11/7",
+              address: "123 This street",
+              state: "Colorado",
+              city: "Denver",
+              zip: 15555,
+              title: "Grandma's basement",
+              description: "We'll be playing caracasonne for 9 hours",
+              host: 4744564,
+              game: 97833646,
+              gameType: "board game"
+            }) {
+              event {
+                id
+                date
+                address
+                state
+                city
+                zip
+                title
+                description
+                hostId
+                game
+                gameType
+              }
             }
           }
-        }
+        GQL
+      end
+
+      def query_missing_required_field
+        <<~GQL
+          mutation {
+            createEvent(input: {
+              date: "2023/11/7",
+              address: "123 This street",
+              state: "Colorado",
+              title: "Grandma's basement",
+              description: "We'll be playing caracasonne for 9 hours",
+              host: 4744564,
+              game: 97833646,
+              gameType: "board game"
+            }) {
+              event {
+                id
+                date
+                address
+                state
+                city
+                zip
+                title
+                description
+                hostId
+                game
+                gameType
+              }
+            }
+          }
         GQL
       end
     end
