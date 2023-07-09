c1bceb64c23802d57747eb1fa49180136bf965f9
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-07-08 18:30:54 -0600

    test: Passing test for event and update to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :user_events
   has_many :events, through: :user_events
 
-  validate_presence_of :username, :password, :city, :state
+  validates_presence_of :username, :password, :city, :state
 end
