3e242d3928ecca3dc0730122af01ba47797206c6
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-10 16:36:20 -0600

    chore: add user_game field
    Co-authored-by: Brian Guthrie <118231210+Brianisthebest@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_event, mutation: Mutations::CreateEvent
     field :create_user_event, mutation: Mutations::CreateUserEvent
+    field :create_user_game, mutation: Mutations::CreateUserGame
     # TODO: remove me
     field :test_field, String, null: false,
                                description: 'An example field added by the generator'
