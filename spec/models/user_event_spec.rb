594f5f338030423836491403ccc4eeab37bd1ee5
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-10 17:58:14 -0600

    test & refactor: add test of previosly untested uniqueness validation
    Co-authored-by: Brian Guthrie <118231210+Brianisthebest@users.noreply.github.com>

--- spec/models/user_event_spec.rb
+++ spec/models/user_event_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe UserEvent, type: :model do
   describe 'instantiation and relationships' do
     it { should belong_to(:user) }
     it { should belong_to(:event) }
+
+    before do
+      @user = create(:user)
+      @event = create(:event, id: 2000)
+      @user_event = UserEvent.create(user_id: @user.id, event_id: @event.id)
+    end
+
+    subject { @user_event }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:event_id) }
   end
 end
