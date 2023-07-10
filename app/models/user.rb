9683734a85d4ff0e44ec4a242b9c7e794588f9ad
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-10 16:40:15 -0600

    chore: added new relationship between users and game through user_games
    Co-authored-by: Brian Guthrie <118231210+Brianisthebest@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :user_games
   has_many :user_events
   has_many :events, through: :user_events
+  has_many :games, through: :user_games
 
   validates_presence_of :username, :password, :city, :state
 
