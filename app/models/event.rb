3831b3938e5215465b071db1f5ae40d6d18320a8
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-07-11 15:29:32 -0600

    sort events by distance to user

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@
 
 # This is the event model
 class Event < ApplicationRecord
-  has_many :user_events
+  has_many :user_events, dependent: :destroy
   has_many :users, through: :user_events
 
   validates_presence_of :date, :address, :city, :state, :zip, :title, :description, :host_id, :game, :game_type
@@ -24,6 +24,6 @@ class Event < ApplicationRecord
   end
 
   def full_address
-    "#{address}, #{city}, #{state} #{zip.to_s.rjust(5, '0')}"
+    "#{address} #{city}, #{state} #{zip.to_s.rjust(5, '0')}"
   end
 end
