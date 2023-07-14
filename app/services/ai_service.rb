70761e32b39e8f254f8f45c51bd2645bd7e48cb7
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-14 17:35:22 -0600

    refactor: change get_games method, remove commented out code

--- app/services/ai_service.rb
+++ app/services/ai_service.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'faraday'
-
+# this is our ai service
 class AiService
   def conn
     @_conn = Faraday.new(url: "https://api.openai.com/v1/chat/completions?") do |connection|
@@ -8,51 +8,26 @@ class AiService
       connection.headers['Authorization'] = "Bearer #{ENV['OPENAI_ACCESS_TOKEN']}"
     end
   end
-  
+
   def get_games(collection)
     payload = {
       'model': 'gpt-3.5-turbo',
       'messages': [
         {
           'role': 'user',
-          'content': "recommend me 3 board games based on the following collection of games. #{collection} 
+          'content': "recommend me 3 board games based on the following collection of games. #{collection}
           #                 Please include only the name of the games, in the form of strings within an array."
         }
       ],
       'temperature': 0.8
     }
 
-    JSON.parse(
+    response = JSON.parse(
       conn.post do |request|
-      request.body = payload.to_json
-    end.body, symbolize_names: true
+        request.body = payload.to_json
+      end.body, symbolize_names: true
     )
+
+    JSON.parse(response[:choices][0][:message][:content])
   end
 end
-
-
-#   def conn
-#     @_conn ||= Faraday.new(url: 'https://api.openai.com/') do |f|
-#       f.headers['Authorization'] = "Bearer #{ENV['OPEN_AI_API_KEY']}" 
-#       f.headers['content-type'] = 'application/json'
-#     end
-#   end
-
-#   def get_games(collection)
-#     params = {
-#       model: "gpt-3.5-turbo",
-      # messages: [
-      #   {
-      #     role: "user",
-      #     content: "recommend me 3 board games based on the following collection of games. #{collection} 
-      #                 Please include only the name of the games, in the form of strings within an array."
-      #   }
-      # ],
-#       temperature: 0.8
-#     }
-
-
-#     response = conn.post('v1/chat/completions', JSON.generate(params))
-# require 'pry'; binding.pry
-#   end
-# end
