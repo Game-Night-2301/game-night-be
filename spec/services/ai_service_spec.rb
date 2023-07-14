0eec3dc041b24227935bef1cbef480dd76519665
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-14 17:37:48 -0600

    refactor: ai service testing

--- spec/services/ai_service_spec.rb
+++ spec/services/ai_service_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe AiService, vcr: { record: :new_episodes } do
     collection = ["Catan", "Betrayal at House on the Hill", "Gloom Haven", "Brass: Birmingham", "Survive: Escape from Atlantis"]
     ai_service = AiService.new.get_games(collection)
 
-    expect(ai_service).to be_a(Hash)
-    expect(JSON.parse(ai_service[:choices][0][:message][:content])).to be_an(Array)
-    expect(JSON.parse(ai_service[:choices][0][:message][:content]).length).to eq(3)
+    expect(ai_service).to be_a(Array)
+    expect(ai_service).to all(be_a(String))
+    expect(ai_service.length).to eq(3)
   end
 end
